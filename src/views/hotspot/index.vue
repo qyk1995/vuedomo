d58dfb85bd8c5d93f799ce8e8fb5e5abb714b58c
qyk19952021 <10143855+qyk19952021@user.noreply.gitee.com>
2022-08-17 16:25:52 +0800

    修改密码

--- src/views/hotspot/index.vue
+++ src/views/hotspot/index.vue
@@ -2,11 +2,11 @@
     <div>
         <div class="queryButton">
             <el-input v-model="input.wallet" placeholder="请输入钱包地址" style="width:300px;margin-right: 10px;"></el-input>
-            <el-button type="primary" @click="queryHospot">查询钱包热点</el-button>
+            <el-button type="primary" :plain="true"  @click="queryHospot">查询钱包热点</el-button>
         </div>
         <div class="mybutton">
-             <el-tooltip class="item" effect="light" content="转移热点前请先查询热点" placement="right-end">
-             <el-button type="primary" plain @click="transferHospot">热点转移</el-button>
+             <el-tooltip class="item" effect="light" content="请选择下列热点地址，未选择下列热点地址需要输入热点地址" placement="right-end">
+             <el-button type="primary" :plain="true" @click="transferHospot">热点转移</el-button>
 
     </el-tooltip>
         </div>
@@ -40,15 +40,25 @@
             :visible.sync="dialogVisible"
             width="50%"
             :before-close="handleClose">
-            <el-form ref="form" :model="form" label-width="180px">
-                
-                <el-form-item label="密码"  :rules="[{ required: true, message: '请输入钱包密码', trigger: 'blur' },]">
+            <el-form ref="form" :model="form"  :rules="rules" label-width="180px">
+                <el-form-item label="密码" prop="password" >
                     <el-input type="password"  v-model="form.password" autocomplete="off"></el-input>
                 </el-form-item>
+
+                <el-form-item label="确认密码" prop="checkPass" >
+                    <el-input type="password" v-model="form.checkPass" autocomplete="off"></el-input>
+                </el-form-item>
+                
                 <el-form-item label="热点所属的钱包地址" :rules="[{ required: true, message: '请输入钱包地址', trigger: 'blur' },]">
                     <el-input v-model="form.wallets"></el-input>
                 </el-form-item>
                 
+
+                <el-form-item   label="自定义热点地址"   :rules="[{ required: true, message: '请输入热点地址,以逗号隔开', trigger: 'blur' },]">
+                    <el-input  :disabled="this.dishostpost"    v-model="form.hotspots"></el-input>
+                </el-form-item>
+
+
                 <el-form-item label="热点所属的钱包key路径"  :rules="[{ required: true, message: '请输入钱包key文件路径', trigger: 'blur' },]">
                     <el-input v-model="form.srckey_paths"></el-input>
                 </el-form-item>
@@ -71,7 +81,7 @@
 
             <span slot="footer" class="dialog-footer">
                 <el-button @click="dialogVisible = false">取 消</el-button>
-                <el-button type="primary" @click="onSubmit">确 定</el-button>
+                <el-button type="primary" @click="onSubmit('form')">确 定</el-button>
             </span>
 
         </el-dialog>
@@ -82,6 +92,25 @@
 import {gethotspotList, updateHotspot} from '../../api'
 export default {
     data(){
+        var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.form.checkPass !== '') {
+            this.$refs.form.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.form.password) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };    
         return{
            tableData: [
                 
@@ -94,9 +123,18 @@ export default {
                 hotspots:[]
             },
             input:{
-
             },
-            file:null
+            rules: {
+                    password: [
+                        { validator: validatePass, trigger: 'blur' }
+                    ],
+                    checkPass: [
+                        { validator: validatePass2, trigger: 'blur' }
+                    ],
+                },
+            file:null,
+            dishostpost: false
+            
         }
     },
     mounted(){
@@ -113,19 +151,30 @@ export default {
           })
           .catch(_ => {});
       },
-      onSubmit(){
-        this.dialogVisible = false
-        let fileFormData = new FormData();
-        console.log(this.file)
-        fileFormData.append("file",this.file)
-        fileFormData.append("password",this.form.password)
-        fileFormData.append("hotspots",this.form.hotspots)
-        fileFormData.append("wallets",this.form.wallets)
-        fileFormData.append("srckey_paths",this.form.srckey_paths)
 
-        this.updateHotspot(fileFormData)
-       // console.log(this.form.count)
-      },
+     onSubmit(formName){
+        this.dialogVisible = false
+
+        this.$refs[formName].validate((valid) => {
+        if (valid) {
+            console.log('submit!!');
+            let fileFormData = new FormData();
+            console.log(this.file)
+            fileFormData.append("file",this.file)
+            fileFormData.append("password",this.form.password)
+            fileFormData.append("hotspots",this.form.hotspots)
+            fileFormData.append("wallets",this.form.wallets)
+            fileFormData.append("srckey_paths",this.form.srckey_paths)
+            this.updateHotspot(fileFormData)
+            
+          } else {
+            console.log('error submit!!');
+            alert('输入信息错误,无法提交!!!');
+            return false;
+          }
+
+        });
+        },
       handleSelectionChange(vil){
         this.form.hotspots = []
          vil.forEach(element => {
@@ -142,6 +191,17 @@ export default {
      async hotspontList(params){
         let res = await gethotspotList(params);
         let hotspontArr = res.data.hotspots;
+         if(res.status == 1){
+             this.$message({
+                message: res.msg,
+                type: 'success'
+            });
+        }else{
+              this.$message({
+                message: res.msg,
+                type: 'success'
+            });
+        }
         hotspontArr.forEach(element => {
             this.tableData.push({hotspont:element})
         });
@@ -158,7 +218,10 @@ export default {
                 type: 'success'
             });
         }else{
-            this.$message.error(res.msg);
+              this.$message({
+                message: res.msg,
+                type: 'success'
+            });
         }
      },
      // 点击查询
@@ -166,13 +229,18 @@ export default {
         this.hotspontList(this.input)
      },
      // 打开热点转移弹窗
-     transferHospot(){
-        if(this.form.hotspots == undefined || this.form.hotspots.length <= 0) {
-            this.$message.error("未选择热点");
-        }else{
+     transferHospot(){        
+      if(this.form.hotspots == undefined || this.form.hotspots.length <= 0) {
+            // this.$message.error("未选择热点");
             this.dialogVisible = true;
-        }
 
+            this.dishostpost = false
+
+        }else{
+           this.dialogVisible = true;
+            this.dishostpost = true
+
+        }
      }
     }
 }
--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
 			'/api': {
 				target:'http://127.0.0.1:5000',
 				changeOrigin:true,
-				pathRewrite: {'^/api': ''}
+				// pathRewrite: {'^/api': ''}
 			},
 		},
 	},
